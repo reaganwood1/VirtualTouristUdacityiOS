080a0caa3e7fa0e7aa0679bd7453cdb16ab9eb3f
Reagan Wood <reaganwood1@gmail.com>
2016-05-28 20:49:38 -0500

    Long Press Gesture code added to the MapViewController

--- VirtualTourist/TravelLocationsViewController.swift
+++ VirtualTourist/TravelLocationsViewController.swift
@@ -7,11 +7,15 @@
 //
 
 import UIKit
+import MapKit
 
-class TravelLocationsViewController: UIViewController {
+class TravelLocationsViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        longPressGestureActivated()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +24,21 @@ class TravelLocationsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func didLongPress() {
+        print("long pressed")
+    }
+    
+    func longPressGestureActivated () {
+        
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: "didLongPress")
+        longPressGesture.minimumPressDuration = 1
+        mapView.addGestureRecognizer(longPressGesture)
+        
+    }
+    
+    func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        
+    }
+    
 }
 
