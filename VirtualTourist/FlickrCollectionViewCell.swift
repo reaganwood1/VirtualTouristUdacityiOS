7c820c251206d24a00fba59f5ceb0c3d2148af37
Reagan Wood <reaganwood1@gmail.com>
2016-06-01 19:57:24 -0500

    Added new VC for posing a possible solution to the PhotoAlbumViewController problem. Also, created new CollectionViewCell which will be used to display information for the the PhotoAlbumView

---
+++ VirtualTourist/FlickrCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  FlickrCollectionViewCell.swift
+//  VirtualTourist
+//
+//  Created by Reagan Wood on 6/1/16.
+//  Copyright © 2016 RW Software. All rights reserved.
+//
+
+import UIKit
+
+class FlickrCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var flickrImage: UIImageView!
+    @IBOutlet weak var flickrActivityIndicator: UIActivityIndicatorView!
+    
+}
---
+++ VirtualTourist/PhotoAlbumMapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoAlbumMapViewController.swift
+//  VirtualTourist
+//
+//  Created by Reagan Wood on 6/1/16.
+//  Copyright © 2016 RW Software. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+import MapKit
+
+class PhotoAlbumMapViewController: UIViewController, UICollectionViewDelegate, MKMapViewDelegate {
+    
+    override func viewDidLoad() {
+        
+    }
+}
